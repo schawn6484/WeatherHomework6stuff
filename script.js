5aa812f5194e16b2a4be36510bba17320c686923
schawn6484 <schawnetta.robinson@gmail.com>
2020-11-07 18:15:22 -0500

    added script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+
+    $('#search-button').click(function(){
+        console.log('button clicked')
+  
+    });  
+    $('#form').submit(function(event){
+event.preventDefault()
+    });
+
+
+
+
+
+
+    
+
+
+const apiKey =''
+});
