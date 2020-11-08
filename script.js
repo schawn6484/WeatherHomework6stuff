adcc783ca65775c979366e3da7a3c5358b88d143
schawn6484 <schawnetta.robinson@gmail.com>
2020-11-07 20:22:41 -0500

    modified script.js

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 $(document).ready(function(){
 
+var apiKey ='5182bbd11c76e6b9d60dae8adc3b808b'
 
     $('#search-button').click(function(){
         console.log('button clicked')
-  
+
+        var city =$('searchValue').val();
+            today(city);
+            getForecast(city);
+            addCity(city);
     });  
+
+    function today(city){
+        $.ajax({
+            url:'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric' +'APPID=5182bbd11c76e6b9d60dae8adc3b808b',
+            type: 'GET',
+            dataType: 'json',
+        })
+    }
+
+
     $('#form').submit(function(event){
 event.preventDefault()
     });
@@ -14,8 +29,8 @@ event.preventDefault()
 
 
 
-    
 
 
-const apiKey =''
+
+
 });
